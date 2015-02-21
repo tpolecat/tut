7b2ddd71472cb9a2d30d9eda61b27e26237a3dfa
Rob Norris <rob_norris@mac.com>
2015-02-20 23:21:19 -0800

    options and plugins

--- plugin/src/main/scala/tut/Plugin.scala
+++ plugin/src/main/scala/tut/Plugin.scala
@@ -7,8 +7,10 @@ import sbt.Attributed.data
 
 object Plugin extends sbt.Plugin {
   
-  lazy val tut = TaskKey[Seq[(File,String)]]("tut", "create tut documentation")
+  lazy val tut                = TaskKey[Seq[(File,String)]]("tut", "create tut documentation")
   lazy val tutSourceDirectory = SettingKey[File]("tutSourceDirectory", "where to look for tut sources")
+  lazy val tutScalacOptions   = TaskKey[Seq[String]]("tutScalacOptions", "scalac options")
+  lazy val tutPluginJars      = TaskKey[Seq[File]]("tutPluginJars", "Plugin jars to be used by tut REPL.")
 
   def safeListFiles(dir: File): List[File] =
     Option(dir.listFiles).fold(List.empty[File])(_.toList)
@@ -19,14 +21,28 @@ object Plugin extends sbt.Plugin {
       libraryDependencies += "org.tpolecat" %% "tut-core" % "0.4.0-SNAPSHOT",
       tutSourceDirectory := sourceDirectory.value / "main" / "tut",
       watchSources <++= tutSourceDirectory map { path => (path ** "*.md").get },
+      tutScalacOptions := (scalacOptions in (Compile, doc)).value,
+      tutPluginJars := {
+        // no idea if this is the right way to do this
+        val deps = (libraryDependencies in (Compile, doc)).value.filter(_.configurations.fold(false)(_.startsWith("plugin->")))
+        update.value.configuration("plugin").map(_.modules).getOrElse(Nil).filter { m =>
+          deps.exists { d => 
+            d.organization == m.module.organization &&
+            d.name         == m.module.name &&
+            d.revision     == m.module.revision
+          }
+        }.flatMap(_.artifacts.map(_._2))
+      },
       tut := {
-        val r   = (runner in (Compile, doc)).value
-        val in  = tutSourceDirectory.value
-        val out = crossTarget.value / "tut"
-        val cp  = (fullClasspath in (Compile, doc)).value
+        val r     = (runner in (Compile, doc)).value
+        val in    = tutSourceDirectory.value
+        val out   = crossTarget.value / "tut"
+        val cp    = (fullClasspath in (Compile, doc)).value
+        val opts  = tutScalacOptions.value
+        val pOpts = tutPluginJars.value.map(f => "–Xplugin:" + f.getAbsolutePath)
         toError(r.run("tut.TutMain", 
                       data(cp), 
-                      Seq(in.getAbsolutePath, out.getAbsolutePath), 
+                      Seq(in.getAbsolutePath, out.getAbsolutePath) ++ opts ++ pOpts, 
                       streams.value.log))
         // We can't return a value from the runner, but we know what TutMain is looking at so we'll
         // fake it here. Returning all files potentially touched.
--- tests/src/sbt-test/tut/test-01-options/build.sbt
+++ tests/src/sbt-test/tut/test-01-options/build.sbt
@@ -1,12 +1,12 @@
 tutSettings
 
-scalaVersion := "2.11.4"
+scalaVersion := sys.props("scala.version")
 
 lazy val check = TaskKey[Unit]("check")
 
 check := {
   val expected = IO.readLines(file("expect.md"))
-  val actual   = IO.readLines(file("target/scala-2.11/tut/test.md"))
+  val actual   = IO.readLines(crossTarget.value / "tut"/ "test.md")
   if (expected != actual) 
     error("Output doesn't match expected: \n" + actual.mkString("\n"))
 }
